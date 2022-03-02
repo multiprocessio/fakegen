5bb02b32039387f5a846c6a9d4a4b512487b893d
Phil Eaton <phil@eatonphil.com>
2022-03-02 18:09:13 +0000

    Fixes and testing

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# fakegen: 
+# fakegen: A single binary CLI for generating fake JSON data
+
+## Installation
+
+```bash
+$ go install https://github.com/multiprocessio/fakegen
+```
+
+## Usage
+
+Pass the number of rows and columns you want and `fakegen` will give
+you a JSON array of objects with that many rows and unique columns.
+
+```
+$ fakegen --rows 10 --cols 1000
+```
--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"os"
-	"time"
 	"encoding/json"
+	"fmt"
 	"log"
-	"rand"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/jaswdr/faker"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	faker := faker.New()
 
-	var rows, columns int
+	var rows, cols int
 	for i, arg := range os.Args {
 		var err error
 		if arg == "--rows" {
 			rows, err = strconv.Atoi(os.Args[i+1])
 			if err != nil {
-				log.Fatal("--rows must be an int. e.g. --rows 10_000")
-			}
-
-			if rows <= 1 {
-				log.Fatal("--rows must be at least 1")
+				log.Fatal("--rows must be an int. e.g. --rows 10000: " + err.Error())
 			}
 
 			i += 1
@@ -33,11 +33,7 @@ func main() {
 		if arg == "--cols" {
 			cols, err = strconv.Atoi(os.Args[i+1])
 			if err != nil {
-				log.Fatal("--cols must be an int. e.g. --cols 10_000")
-			}
-
-			if cols <= 1 {
-				log.Fatal("--cols must be at least 1")
+				log.Fatal("--cols must be an int. e.g. --cols 10000: " + err.Error())
 			}
 
 			i += 1
@@ -45,40 +41,44 @@ func main() {
 		}
 	}
 
+	if rows < 1 {
+		log.Fatal("--rows must be at least 1")
+	}
+
+	if cols < 1 {
+		log.Fatal("--cols must be at least 1")
+	}
+
 	// Populate all the column names
-	var schema map[string]func()interface{}
-	types := []func()interface{}{
-		faker.Lorem().Paragraph,
-		faker.Int32,
-		faker.Lorem().Word,
-		faker.Lorem().Image,
-		faker.Lorem().Food,
-		faker.Lorem().Color,
-		faker.Lorem().Car,
+	schema := map[string]func() interface{}{}
+	types := []func() interface{}{
+		func() interface{} {
+			return faker.Lorem().Paragraph(rand.Intn(3) + 1)
+		},
+		func() interface{} {
+			return faker.Int32()
+		},
+		func() interface{} {
+			return strings.Join(faker.Lorem().Words(rand.Intn(10)+1), " ")
+		},
+		func() interface{} {
+			return faker.UInt16()
+		},
 	}
-outer:
-	for {
-		if len(schema) == cols {
-			break
-		}
-
-		w := faker.Lorem().Word()
-		_, exists := keys[w]
-		if !exists {
-			schema[w] = types[rand.Intn(len(types))]
-		}
+	for i := 0; i < cols; i += 1 {
+		schema[fmt.Sprintf("col%d", i)] = types[rand.Intn(len(types))]
 	}
 
-	_, err := os.Stdout.Write([]byte("]"))
+	_, err := os.Stdout.Write([]byte("["))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	encoder := json.Encode(os.Stdout)
+	encoder := json.NewEncoder(os.Stdout)
 	row := map[string]interface{}{}
 	for i := 0; i < rows; i += 1 {
 		if i > 0 {
-			_, err := os.Stdout.Write([]byte("]"))
+			_, err := os.Stdout.Write([]byte(",\n"))
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -93,7 +93,7 @@ outer:
 		}
 	}
 
-	_, err := os.Stdout.Write([]byte("]"))
+	_, err = os.Stdout.Write([]byte("]"))
 	if err != nil {
 		log.Fatal(err)
 	}
